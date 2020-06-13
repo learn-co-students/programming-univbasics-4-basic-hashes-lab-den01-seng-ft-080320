482532b26a6a7a7ca052b22eb3837942ffdfb784
Jennifer Grenier <jenniferagrenier@gmail.com>
2020-06-13 01:09:19 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,25 +1,17 @@
 def new_hash
-  new_hash = Hash.new
+  {}
 end
 
 def my_hash
   my_hash = {
-    :dogs => 3,
-    :cats =>2
+    dogs: 2
   }
 end
 
 def pioneer
-  pioneer_hash={
-    name: 'Grace Hopper'
-  }
+  {name: 'Grace Hopper'}
 end
 
-
 def id_hash_generator(number)
-  hash= {
-    :id => number
-  }
-
-  # return a hash with a key :id assigned to the provided number
+  {id:number}
 end
